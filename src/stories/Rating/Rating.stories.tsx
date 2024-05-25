6c0dd03141d8470dcae3ddf1db7ccc80302ed26b
ntzolov <ntzolov@gmail.com>
2024-05-25 15:38:36 +0300

    create rating component

---
+++ src/stories/Rating/Rating.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Rating from './Rating';
+
+const meta = {
+  title: 'Example/Rating',
+  component: Rating,
+} satisfies Meta<typeof Rating>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    number: 5,
+  },
+};
---
+++ src/stories/Rating/Rating.tsx
@@ -0,0 +1,24 @@
+import './Rating.css';
+import { MouseEventHandler, useState } from 'react';
+import { FaStar } from 'react-icons/fa';
+
+export type StarProps = {
+  selected: boolean;
+  onSelect?: MouseEventHandler<SVGElement>;
+};
+
+export type RatingProps = {
+  number?: number;
+};
+
+const Star = ({ selected, onSelect = (f) => f }: StarProps) => (
+  <FaStar className='star' color={selected ? '#ffcb45' : '#f2f2f2'} onClick={onSelect} />
+);
+
+export default function Rating({ number = 10 }: RatingProps) {
+  const [selected, setSelected] = useState(0);
+
+  return [...Array(number)].map((_, i) => (
+    <Star key={i} onSelect={() => setSelected(i + 1)} selected={i < selected} />
+  ));
+}
