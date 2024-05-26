a4ced2696a793e6f2c1ddef6ebc6d176059c03af
ntzolov <ntzolov@gmail.com>
2024-05-26 17:08:45 +0300

    Add separators between the components

--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -5,6 +5,7 @@ import './page.css';
 import Counter from './Counter/Counter';
 import Rating from './Rating/Rating';
 import TextInput from './TextInput/TextInput';
+import Separator from './Separator/Separator';
 
 type User = {
   name: string;
@@ -22,10 +23,14 @@ export const Page: React.FC = () => {
         onCreateAccount={() => setUser({ name: 'Jane Doe' })}
       />
 
+      <Separator margin={10} />
+
       <div className='counter-component'>
         <p>Counter component:</p>
         <Counter backgroundColor='#535559' color='#d5d6de' />
       </div>
+
+      <Separator margin={10} />
 
       <div className='rating-component'>
         <p>Rating component:</p>
@@ -34,11 +39,18 @@ export const Page: React.FC = () => {
         </div>
       </div>
 
+      <Separator margin={10} />
+
       <div className='input-component'>
         <p>Input component:</p>
         <TextInput name='name' type='text' label='Name' placeholder='Enter your name...' />
       </div>
-      <h1 className='text-blue-700 font-bold text-3xl'>Tailwind added 🇧🇬</h1>
+
+      <Separator margin={10} />
+
+      <h1 className='text-violet-600 font-bold text-3xl'>Tailwind added 🇧🇬</h1>
+
+      <Separator margin={10} />
     </article>
   );
 };
